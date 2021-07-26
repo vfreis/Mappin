dab02474951bff74bd93361918ec6793b2c108a2
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2021-07-25 23:16:23 -0300

    method new Pin created

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const app = express()
 
 dotenv.config()
 
+app.use(express.json())
+
 mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
---
+++ backend/routes/pins.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+const Pin = require('../models/Pin')
+
+// Create Pin \/
+
+router.post('/', async (req, res)=>{
+    const newPin = new Pin(req.body)
+    try{
+        const savedPin = await newPin.save()
+        res.status(200).json(savedPin)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+// Get all Pins \/
