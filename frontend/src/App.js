b29dda4d32086c62201045e34ae1138d7cf28d08
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2021-07-27 11:44:31 -0300

    MapBox Api implemented

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
+
+function App() {
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
+
+
+  return (
+    <div className="App">
+      <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+    />
+    </div>
+  );
+}
+
+export default App;
