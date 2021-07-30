c626d47a6ae6b674de6583623523452b39ee8374
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2021-07-30 16:23:25 -0300

    timeago.js added; pin cards and close button ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,11 @@ import ReactMapGL, {Marker, Popup} from 'react-map-gl';
 import {Room, Star} from '@material-ui/icons';
 import './app.css';
 import axios from 'axios'
+import {format} from 'timeago.js'
 
 function App() {
 	const [pins, setPins] = useState([])
+	const [currentPlaceId, setCurrentPlaceId] = useState(null)
   	const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -26,6 +28,10 @@ function App() {
 	getPins()
   }, [])
 
+  const handleMarkerClick = (id)=>{
+	setCurrentPlaceId(id);
+  }
+
   return (
     <div className="App">
       <ReactMapGL
@@ -43,14 +49,20 @@ function App() {
 			  offsetTop={-10}
 			  >
 
-          <Room style={{fontSize: viewport.zoom * 7, color: 'slateblue'}}/>
+          <Room style={{fontSize: viewport.zoom * 7, color: 'slateblue'
+		  }}
+		  onClick={()=>handleMarkerClick(p._id)}
+		  />
         </Marker>
+		{p._id === currentPlaceId && (
 		<Popup
 			latitude={p.lat}
 			longitude={p.long}
 			closeButton={true}
 			closeOnClick={false}
-			anchor="left" >
+			anchor="left" 
+			onClose={()=>setCurrentPlaceId(null)}
+			>
 			<div className="card">
 			<label>Place</label>
 			<h4 className="place">{p.title}</h4>
@@ -66,11 +78,13 @@ function App() {
 			</div>
 			<label>Information</label>
 			<span className="username">Created by <b>{p.username}</b> </span>
-			<span className="date">1 Hour ago</span>
+			<span className="date">{format(p.createdAt)}</span>
 			
 			</div>
-        </Popup> </>
-		  ))}
+        </Popup> 
+		)}
+	</>
+))}
 		</ReactMapGL>
     </div>
   );
