27c2473bb82f849cc76d3f2c4ea57140ca60f407
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2021-07-31 15:44:30 -0300

    post method to add pins through double click

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,14 @@ import axios from 'axios'
 import {format} from 'timeago.js'
 
 function App() {
+	const currentUser = 'Jane'
 	const [pins, setPins] = useState([])
 	const [currentPlaceId, setCurrentPlaceId] = useState(null)
-  	const [viewport, setViewport] = useState({
+	const [newPlace, setNewPlace] = useState(null)
+	const [title, setTitle] = useState(null)
+	const [desc, setDesc] = useState(null)
+	const [rating, setRating] = useState(0)
+	const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
     latitude: 47.040182,
@@ -28,8 +33,39 @@ function App() {
 	getPins()
   }, [])
 
-  const handleMarkerClick = (id)=>{
+  const handleMarkerClick = (id, lat, long)=>{
 	setCurrentPlaceId(id);
+	setViewport({...viewport, latitude: lat, longitude: long})
+  }
+  
+  const handleAddClick = (e) => {
+	const [long, lat] = e.lngLat
+	setNewPlace({
+		lat,
+		long
+	})
+  }
+
+  const handleSubmit = async (e) => {
+	  e.preventDefault()
+	  const newPin = {
+		  username: currentUser,
+		  title,
+		  desc,
+		  rating,
+		  lat:newPlace.lat,
+		  long:newPlace.long
+	  }
+
+	  try{
+		
+		const res = await axios.post('/pins', newPin)
+		setPins([...pins, res.data])
+		setNewPlace(null)
+		
+	  } catch(err) {
+		console.log(err)
+	  }
   }
 
   return (
@@ -39,6 +75,8 @@ function App() {
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
       onViewportChange={nextViewport => setViewport(nextViewport)}
 	  mapStyle="mapbox://styles/mariofromhell/ckrm7i5se0nhm18ly33izm5ar"
+	  onDblClick = {handleAddClick}
+	  transitionDuration="200"
       >
 		  {pins.map((p)=>(
 			<>
@@ -49,9 +87,9 @@ function App() {
 			  offsetTop={-10}
 			  >
 
-          <Room style={{fontSize: viewport.zoom * 7, color: 'slateblue'
+          <Room style={{fontSize: viewport.zoom * 7, color: p.username===currentUser?'tomato' : 'slateblue', cursor:"pointer"
 		  }}
-		  onClick={()=>handleMarkerClick(p._id)}
+		  onClick={()=>handleMarkerClick(p._id, p.lat, p.long)}
 		  />
         </Marker>
 		{p._id === currentPlaceId && (
@@ -84,7 +122,43 @@ function App() {
         </Popup> 
 		)}
 	</>
-))}
+	))}
+	{newPlace && (
+		<Popup
+				latitude={newPlace.lat}
+				longitude={newPlace.long}
+				closeButton={true}
+				closeOnClick={true}
+				anchor="left" 
+				onClose={()=>setNewPlace(null)}
+				>
+					<div>
+						 <form onSubmit={handleSubmit}>
+							 <label>Title</label>
+							 <input 
+							 	placeholder="Enter a title" 
+							 	onChange={(e) => setTitle(e.target.value)}
+							 />
+							 <label>Review</label>
+							 <textarea 
+							 	placeholder="What's your review about this place?"
+								onChange={(e) => setDesc(e.target.value)}
+							/>
+							 <label>Rating</label>
+							 <select onChange={(e) => setRating(e.target.value)}>
+								 <option value="1">1</option>
+								 <option value="2">2</option>
+								 <option value="3">3</option>
+								 <option value="4">4</option>
+								 <option value="5">5</option>
+							 </select>
+							 <button className="submitButton" type="submit">Add Pin</button>
+						 </form>
+					</div>
+				</Popup>
+
+	)}
+
 		</ReactMapGL>
     </div>
   );
